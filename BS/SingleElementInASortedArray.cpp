ab17126758f9d742393688e5d4f3c02694d36f8a
srishti666 <srishtimanna6@gmail.com>
2025-12-24 08:14:06 +0530

    easy solution

--- BS/SingleElementInASortedArray.cpp
+++ BS/SingleElementInASortedArray.cpp
@@ -25,3 +25,26 @@ public:
         return nums[low];
     }
 };
+
+
+//easy solution
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        
+        int low=0, high = n-1;
+        
+        while(low<high){
+            
+            int mid = low + (high-low)/2;
+
+            if((mid % 2 == 1 && nums[mid] == nums[mid-1]) || (mid % 2 == 0 && nums[mid] == nums[mid+1])) low=mid+1;
+            
+            else high = mid;
+            
+        }
+        return nums[low];
+    }
+};
