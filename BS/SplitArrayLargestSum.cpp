4470b245e84bd839450869aa205e47e9504887d0
srishti666 <srishtimanna6@gmail.com>
2026-01-08 08:09:28 +0530

    optimized

--- BS/SplitArrayLargestSum.cpp
+++ BS/SplitArrayLargestSum.cpp
@@ -48,12 +48,49 @@ public:
     }
 };
 
+//optimized solution with right variables
+
+class Solution {
+    int countPartitions(vector<int>& nums, int maxSumLimit){
+        long long currentSubarraySum = 0;
+        int partitions = 1;
+
+        for(int i=0; i<nums.size(); i++){
+            if(currentSubarraySum + nums[i] <= maxSumLimit){
+                currentSubarraySum += nums[i];
+            }
+            else{
+                partitions++;
+                currentSubarraySum = nums[i];
+            }
+        }
+        return partitions;
+    }
+public:
+    int splitArray(vector<int>& nums, int k) {
+        int low = *max_element(nums.begin(), nums.end());
+        int high = accumulate(nums.begin(), nums.end(), 0);
+
+        while(low <= high){
+            int mid = low+(high - low)/2;
+
+            int partitions = countPartitions(nums, mid);
+
+            if(partitions > k){
+                low = mid + 1;
+            } 
+            else high = mid - 1 ;
+        }
+        return low;
+    }
+};
+
 
 //striver solution : https://www.youtube.com/watch?v=thUd_WJn6wk
 
 //O(log(sum-max)*n)
 
-
+// book allocation
 class Solution {
 public:
     int countStudents(vector<int>& nums, int pages){
