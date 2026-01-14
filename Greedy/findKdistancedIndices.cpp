dc43b5fd4ba2e5f7e2d80ef9f94ae81da3d6dc6f
srishti666 <srishtimanna6@gmail.com>
2026-01-14 08:17:22 +0530

    solution

--- Greedy/findKdistancedIndices.cpp
+++ Greedy/findKdistancedIndices.cpp
@@ -3,3 +3,19 @@
 
 //Brute force
 //TC : O(n*n)
+
+class Solution {
+public:
+    vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+        vector<int> ans;
+        for(int i=0; i<nums.size(); i++){
+            for(int j=0; j<nums.size(); j++){
+                if(nums[j] == key && abs(i-j) <= k){
+                    ans.push_back(i); 
+                    break;  
+                }
+            }
+        }
+        return ans;
+    }
+};
