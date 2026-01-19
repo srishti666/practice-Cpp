62ff6e7d7ea09e5c9a10ec3356263b785d3a8145
srishti666 <srishtimanna6@gmail.com>
2026-01-19 10:28:01 +0530

    optimized

--- Greedy/kthLargestIntegerInArray.cpp
+++ Greedy/kthLargestIntegerInArray.cpp
@@ -1,2 +1,17 @@
 //1985. Find the Kth Largest Integer in the Array
 //https://leetcode.com/problems/find-the-kth-largest-integer-in-the-array/description/
+
+class Solution {
+public:
+    string kthLargestNumber(vector<string>& nums, int k) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end(), [](const string& a, const string& b) {
+            if (a.size() != b.size()) {
+                return a.size() < b.size();
+            }
+            return a < b;
+        });
+
+        return nums[n - k];
+    }
+};
