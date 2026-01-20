1d219131fb2e21994857aaf3ed5af17837e4524a
srishti666 <srishtimanna6@gmail.com>
2026-01-20 10:56:32 +0530

    brute force

--- Greedy/NonDecreasingArray.cpp
+++ Greedy/NonDecreasingArray.cpp
@@ -1,2 +1,28 @@
 //665. Non-decreasing Array
 //https://leetcode.com/problems/non-decreasing-array/description/
+
+
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int count = 0;
+        int n = nums.size();
+
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                count++;
+                
+                if (count > 1) return false;
+
+                if (i == 0 || nums[i - 1] <= nums[i + 1]) {
+                    nums[i] = nums[i + 1];
+                } 
+                else {
+                    nums[i + 1] = nums[i];
+                }
+            }
+        }
+
+        return true;
+    }
+};
