855d3ec73eb770f9bdbe0d6050c68ca26e1d6af9
srishti666 <srishtimanna6@gmail.com>
2026-02-02 08:28:19 +0530

    code

--- Trees/MaxDepthInBinaryTree.cpp
+++ Trees/MaxDepthInBinaryTree.cpp
@@ -1,2 +1,14 @@
 //104. Maximum Depth of Binary Tree
 //https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL) return 0;
+
+        int lh = maxDepth(root->left);
+        int rh = maxDepth(root->right);
+
+        return 1 + max(lh, rh);
+    }
+};
