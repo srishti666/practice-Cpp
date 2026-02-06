0466e1ef816d82aa278b110a63a77823a65332c0
srishti666 <srishtimanna6@gmail.com>
2026-02-06 07:26:11 +0530

    code

--- Trees/SameTree.cpp
+++ Trees/SameTree.cpp
@@ -1,3 +1,27 @@
 //100. Same Tree
 //https://leetcode.com/problems/same-tree/description/
 
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == NULL && q == NULL) 
+            return true;
+
+        if(p != NULL && q == NULL) 
+            return false;
+         
+        if(p == NULL && q!= NULL) 
+            return false;
+         
+        bool left = isSameTree(p->left, q->left);
+        bool right = isSameTree(p->right, q->right);
+
+        bool value = p->val == q->val;
+
+        if(value && left && right) 
+            return true;
+         
+            
+        else return false;
+    }
+ };
