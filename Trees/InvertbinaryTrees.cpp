f1dbf82a9d113d83824ea2908510c8ce5e27ce3e
srishti666 <srishtimanna6@gmail.com>
2026-02-15 08:50:28 +0530

    Code

--- Trees/InvertbinaryTrees.cpp
+++ Trees/InvertbinaryTrees.cpp
@@ -1,2 +1,21 @@
 //226. Invert Binary Tree
 //https://leetcode.com/problems/invert-binary-tree/description/
+
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+
+        if(root == NULL)
+        return 0;
+
+        TreeNode* temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+
+        invertTree(root->right);
+        invertTree(root->left);
+
+        return root;
+    }
+};
