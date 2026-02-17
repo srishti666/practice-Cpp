c5cc0d28718ab277667da1949fb6b256e9559f35
srishti666 <srishtimanna6@gmail.com>
2026-02-17 08:35:47 +0530

    sol

--- Linked Lists/ReverseLL.cpp
+++ Linked Lists/ReverseLL.cpp
@@ -1,3 +1,32 @@
 //206. Reverse Linked List
 
 //https://leetcode.com/problems/reverse-linked-list/description/
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+         stack<int> st;
+
+        // Temporary pointer to traverse the list
+        ListNode* temp = head;
+
+        // Traverse and push all node values to stack
+        while (temp != NULL) {
+            st.push(temp->val);
+            temp = temp->next;
+        }
+
+        // Reset temp back to head
+        temp = head;
+
+        // Reassign values from stack in reverse order
+        while (temp != NULL) {
+            temp->val = st.top();
+            st.pop();
+            temp = temp->next;
+        }
+
+        // Return the modified head
+        return head;
+    }
+};
