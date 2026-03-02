d6e0e0911de29f3c1d131cd34b061baff329c5b9
srishti666 <srishtimanna6@gmail.com>
2026-03-02 09:16:32 +0530

    optimal

--- Linked Lists/LinkedListCycle.cpp
+++ Linked Lists/LinkedListCycle.cpp
@@ -1,6 +1,7 @@
 //141. Linked List Cycle
 //https://leetcode.com/problems/linked-list-cycle/
 //TC: O(n). SC: O(n)
+//brute force
 
 class Solution {
 public:
@@ -14,6 +15,27 @@ public:
             }
             mpp[temp] = true;     
             temp = temp->next;
+        }
+        return false;
+    }
+};
+
+
+//Optimal 
+
+
+//TC : O(n)  SC: O(1)
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast) return true;
         }
         return false;
     }
